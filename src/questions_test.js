d9c8f46e396a421ba016ababd5d3b4cfb85b7e87
alessiaskia <a.scaccia@interface3.be>
2020-11-26 20:18:30 +0100

    completed questions, updated formatting

--- src/questions_test.js
+++ src/questions_test.js
@@ -1,249 +1,300 @@
 export const questionsWAD = [
+
   {
     answers: ['Python', 'Java', 'C++'],
+    bonneReponse: 'Python',
     difficulty: '1',
     question: "Quel langage parmi les suivants est 'faiblement typé' ?",
   },
 
   {
     answers: ['C#', 'Javascript', 'Python'],
+    bonneReponse: 'C#',
     difficulty: '1',
     question: "Quel langage parmi les suivants est 'fortement typé' ?",
   },
 
   {
     answers: ['const', 'static', 'define'],
+    bonneReponse: 'const',
     difficulty: '1',
     question: "En C#, pour spécifier que la valeur d'une variable ne peut pas changer, on la déclare comme une constante avec le mot réservé :",
   },
 
   {
     answers: ['//', '?', '()'],
+    bonneReponse: '//',
     difficulty: '1',
     question: 'Pour avoir un commentaire sur une ligne en C#, Javascript et PHP, on place en début de ligne le(s) caractère(s) :',
   },
 
   {
-    answers: ['PHP', 'C#', 'Python'],
-    difficulty: '1',
-    question: 'Quel langage a été créé en 1994 par Rasmus Lerdorf ?',
-  },
-
-  {
     answers: ['un éléphant', 'une balène', 'une giraffe'],
+    bonneReponse: 'un éléphant',
     difficulty: '1',
     question: "Quel est l'animal mascotte de PHP ?",
   },
 
   {
     answers: ['permet de répéter une instruction', "permet d'afficher un message si une erreur est declenchée"],
+    bonneReponse: 'permet de répéter une instruction',
     difficulty: '1',
     question: "Que fait une 'boucle' ?",
   },
 
   {
     answers: ['Java', 'PHP', 'C#'],
+    bonneReponse: 'Java',
     difficulty: '1',
     question: "Quel langage on n'apprend pas au cours de la formation WAD chez Interface3 ?",
   },
 
   {
-    answers: ['string', 'ulong', 'char'],
-    difficulty: '3',
-    question: "En C#, quel type parmi le suivants n'est pas un 'type primitif' ?",
-  },
-
-  {
-    answers: ['private', 'public', 'protected'],
-    difficulty: '3',
-    question: "Quel est le niveau d'accessibilité par défaut d'une 'class' en C# ?",
-  },
-
-  {
-    answers: ['Console.ReadLine()', 'ExecuteReader()'],
-    difficulty: '3',
-    question: "Travaillant en .NET Framework, qu'est-ce qu'il faut ajouter à la fin du programme pour que la console s'exécute correctement ?",
-  },
-
-  {
-    answers: ['override', 'new', 'static'],
-    difficulty: '3',
-    question: 'En C#, quel mot-clé utilise-t-on pour redéfinir la méthode dans la classe dérivée ? ',
-  },
-
-  {
-    answers: ['false', 'true'],
-    difficulty: '3',
-    question: 'PHP est considéré un langage déclaratif',
-  },
-
-  {
-    answers: ['des serveurs webs', 'des framework Javascript', 'des langages de programmation'],
-    difficulty: '3',
-    question: 'Que sont Apache et IIS ?',
-  },
-
-  {
     answers: ['true', 'false'],
+    bonneReponse: 'true',
     difficulty: '2',
     question: 'C# est destiné à développer sur la plateforme .NET',
   },
 
   {
     answers: ['Visual Studio', 'Jbuilder', 'Sublime Text'],
+    bonneReponse: 'Visual Studio',
     difficulty: '2',
     question: "Quel est l'IDE que Microsoft recommande pour développer en C# ?",
   },
 
   {
     answers: ["libérer de l'espace mémoire en déterminant quels o… sont plus utilisés et peuvent donc être éliminés", "éliminer les liens d'héritage qui ne sont plus nécessaires suite à la création d'une interface", 'détecter les erreurs dans le code et déclancher une erreur'],
+    bonneReponse: "libérer de l'espace mémoire en déterminant quels objets ne sont plus utilisés et peuvent donc être éliminés",
     difficulty: '2',
     question: "Quelle est la fonction du 'garbage collector' ?",
   },
 
   {
     answers: ['false', 'true'],
+    bonneReponse: 'false',
     difficulty: '2',
     question: 'En PHP, toutes les variables doivent être précédées par un # (ex: #myVar)',
   },
 
   {
     answers: ['2002', '2007', '1999'],
+    bonneReponse: '2002',
     difficulty: '2',
     question: 'Dans quelle année Microsoft a présenté la plateforme .NET et le langage C# ?',
   },
 
   {
     answers: ['un framework PHP', 'une librairie Javascript'],
+    bonneReponse: 'un framework PHP',
     difficulty: '2',
     question: "Qu'est-ce que c'est Symphony ?",
   },
 
   {
     answers: ["echo 'Hello World';", "print_message 'Hello World';", "convertToString 'Hello World';"],
+    bonneReponse: "echo 'Hello World';",
     difficulty: '2',
     question: "Comment affiche-t-on 'Hello World' en PHP ?",
   },
+
+  {
+    answers: ['PHP', 'C#', 'Python'],
+    bonneReponse: 'PHP',
+    difficulty: '3',
+    question: 'Quel langage a été créé en 1994 par Rasmus Lerdorf ?',
+  },
+
+  {
+    answers: ['string', 'ulong', 'char'],
+    bonneReponse: 'string',
+    difficulty: '3',
+    question: "En C#, quel type parmi le suivants n'est pas un 'type primitif' ?",
+  },
+
+  {
+    answers: ['private', 'public', 'protected'],
+    bonneReponse: 'private',
+    difficulty: '3',
+    question: "Quel est le niveau d'accessibilité par défaut d'une 'class' en C# ?",
+  },
+
+  {
+    answers: ['Console.ReadLine()', 'ExecuteReader()'],
+    bonneReponse: 'Console.ReadLine()',
+    difficulty: '3',
+    question: "Travaillant en .NET Framework, qu'est-ce qu'il faut ajouter à la fin du programme pour que la console s'exécute correctement ?",
+  },
+
+  {
+    answers: ['override', 'new', 'static'],
+    bonneReponse: 'override',
+    difficulty: '3',
+    question: 'En C#, quel mot-clé utilise-t-on pour redéfinir la méthode dans la classe dérivée ? ',
+  },
+
+  {
+    answers: ['false', 'true'],
+    bonneReponse: 'false',
+    difficulty: '3',
+    question: 'PHP est considéré un langage déclaratif',
+  },
+
+  {
+    answers: ['des serveurs webs', 'des framework Javascript', 'des langages de programmation'],
+    bonneReponse: 'des serveurs webs',
+    difficulty: '3',
+    question: 'Que sont Apache et IIS ?',
+  },
 ];
+
 export const questionsWEB = [
+
   {
-    answers: ['chatAt()', 'blabla'],
+    answers: ['id', 'class'],
+    bonneReponse: 'id',
     difficulty: '1',
-    question: 'Which of the following function of String object returns the character at the specified index?',
+    question: "Quel type de selecteur est précédé par '#' (comme #myDiv) ?",
   },
 
   {
-    answers: ['myFunction()', 'blabla'],
+    answers: ['var', 'pull', 'return'],
+    bonneReponse: 'var',
     difficulty: '1',
-    question: "How do you call the function 'myFunction'?",
+    question: 'Quel mot-clé est utilisé pour créer une variable en JavaScript ?',
   },
 
   {
-    answers: ['nav', 'blabla'],
+    answers: ['<aside> ', '<article>', '<header>'],
+    bonneReponse: '<aside> ',
     difficulty: '1',
-    question: 'Which HTML5 element defines navigation links?',
+    question: 'En HTML5, quelle nouvelle balise de section permet de regrouper un contenu tangentiel au contenu principal du document ?',
   },
 
   {
-    answers: ['8px', 'blabla'],
+    answers: ['nav', 'footer', 'prompt'],
+    bonneReponse: 'nav',
     difficulty: '1',
-    question: 'what value is given for the left margin:',
+    question: 'Dans quel élément HTML5 on peut trouver les liens de navigations ?',
   },
 
   {
-    answers: ['true', 'blabla'],
+    answers: ['false', 'true'],
+    bonneReponse: 'false',
     difficulty: '1',
-    question: "HTML ID's can only be used once",
+    question: 'Java et Javascript sont la même chose',
   },
 
   {
-    answers: ['color', 'blabla'],
+    answers: ['une librairie', 'un framework', 'un server web'],
+    bonneReponse: 'une librairie',
     difficulty: '1',
-    question: 'What property is used to change the text color of an element?',
+    question: "Qu'est-ce que c'est jQuery?",
   },
 
   {
-    answers: ['false', 'blabla'],
+    answers: ['false', 'true'],
+    bonneReponse: 'false',
     difficulty: '1',
-    question: 'Are CSS property names case-sensitive?',
+    question: 'Une classe en HTML5 devrait être utilisée une seule fois',
   },
 
   {
-    answers: ['margin:0 auto;', 'blabla'],
+    answers: ['false', 'true'],
+    bonneReponse: 'false',
     difficulty: '2',
-    question: 'Which snippet of CSS is commonly used to center a website horizontally?',
+    question: 'Les propriétés CSS sont-elles sensibles à la casse ?',
   },
 
   {
-    answers: ['false', 'blabla'],
+    answers: ['array', 'object', 'string'],
+    bonneReponse: 'array',
     difficulty: '2',
-    question: 'JavaScript is the same as Java.',
+    question: 'Quel est le type de cette variable : var a = []; ?',
   },
 
   {
-    answers: ['id', 'blabla'],
+    answers: ['script', 'track', 'include'],
+    bonneReponse: 'script',
     difficulty: '2',
-    question: 'The # symbol specifies that the selector is?',
-  },
-
-  {
-    answers: ['!DOCTYPE html', 'blabla'],
-    difficulty: '2',
-    question: 'Which doctype is correct for HTML5?',
-  },
-
-  {
-    answers: ['object', 'blabla'],
-    difficulty: '2',
-    question: "var a = []; What does 'typeof a' return?",
-  },
-
-  {
-    answers: ['script', 'blabla'],
-    difficulty: '2',
-    question: 'Inside which HTML element do we put the JavaScript?',
-  },
-
-  {
-    answers: ['var', 'blabla'],
-    difficulty: '2',
-    question: 'What keyword is used to create a JavaScript variable',
-  },
-
-  {
-    answers: ['double', 'boolean', 'undefined'],
-    difficulty: '2',
-    question: 'Which is not a JavaScript data type?',
-  },
-
-  {
-    answers: ['a library', 'blabla'],
-    difficulty: '3',
-    question: 'What is jQuery?',
-  },
-
-  {
-    answers: ['pop()', 'blabla'],
-    difficulty: '3',
-    question: 'Which of the following function of Array object removes the last element from an array and returns that element?',
+    question: "Quel élément HTML permet d'inclure les fichiers JavaScript ?",
   },
 
   {
     answers: ['blink', 'section', 'header'],
-    difficulty: '3',
-    question: 'What is not an HTML5 element?',
+    bonneReponse: 'blink',
+    difficulty: '2',
+    question: "Quel élément parmi les suivants n'est pas un élément HTML5 ?",
   },
 
   {
-    answers: ['false', 'blabla'],
-    difficulty: '3',
-    question: 'Does setting margin-top and margin-bottom have an affect on an inline element?',
+    answers: ['une instruction au début des documents SGML et XML spécifiant  les règles de sa syntaxe', "une commande pour identifier la langue d'un site", 'un type de data HTML5'],
+    bonneReponse: 'une instruction au début des documents SGML et XML spécifiant  les règles de sa syntaxe',
+    difficulty: '2',
+    question: "Qu'est ce-que c'est un doctype?",
   },
 
   {
-    answers: ['rien', 'blabla'],
+    answers: ['myFunction()', 'myFunction.append', 'script.myFunction'],
+    bonneReponse: 'myFunction()',
+    difficulty: '2',
+    question: "Comment appelle-t-on la fonction 'myFunction' ?",
+  },
+
+  {
+    answers: ["console.log('Coucou');", "console.print('Coucou');", 'toConsole(Coucou)'],
+    bonneReponse: "console.log('Coucou');",
+    difficulty: '2',
+    question: "Comment afficher 'Coucou' à la console en Javascript ?",
+  },
+
+  {
+    answers: ['color', 'text-nuance', 'text-color'],
+    bonneReponse: 'color',
     difficulty: '3',
-    question: 'Que fait un doctype?',
+    question: 'Quelle propriété CSS change la couleur du texte ?',
+  },
+
+  {
+    answers: ["une methode qui permet de communiquer avec le serv…plan pendant que la page est affichée à l'écran. ", "une fonctionnalité qui permet d'afficher plusieurs… des requetes SQL sans l'aide de code Javascript.", 'une équipe de foot du Suriname'],
+    bonneReponse: "une methode qui permet de communiquer avec le serveur à l'aide de code Javascript en arrière-plan pendant que la page est affichée à l'écran. ",
+    difficulty: '3',
+    question: "Qu'est ce-que c'est Ajax ?",
+  },
+
+  {
+    answers: ['double', 'boolean', 'undefined'],
+    bonneReponse: 'double',
+    difficulty: '3',
+    question: "Quel parmi les choix suivants n'est pas un type de data JavaScript ?",
+  },
+
+  {
+    answers: ['false', 'true'],
+    bonneReponse: 'false',
+    difficulty: '3',
+    question: 'En CSS, on peut utiliser margin-top et margin-bottom pour modifier un élément inline ?',
+  },
+
+  {
+    answers: ['Internet Explorer 9 ', 'Internet Explorer 7', 'aucune'],
+    bonneReponse: 'Internet Explorer 9 ',
+    difficulty: '3',
+    question: "À partir de quelle version d'Internet Explorer peut-on utiliser nativement les éléments de section HTML5 (sans hack ou script complémentaire) ?",
+  },
+
+  {
+    answers: ["<progress value='50' max='100'>50%</progress> ", "<progress length='100' max='100'>50</progress>"],
+    bonneReponse: "<progress value='50' max='100'>50%</progress> ",
+    difficulty: '3',
+    question: "Comment représenter une barre de progression à 50% d'avancement ?",
+  },
+
+  {
+    answers: ['un framework Javascript', 'un framework PHP', 'une librairie CSS'],
+    bonneReponse: 'un framework Javascript',
+    difficulty: '3',
+    question: "Qu'est-ce que c'est React ?",
   },
 
 ];
