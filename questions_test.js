6bcebbaceeb64a78b5efb42033f8abada4694a8b
alessiaskia <a.scaccia@interface3.be>
2020-11-26 12:28:35 +0100

    added list of questions with right formatting

---
+++ questions_test.js
@@ -0,0 +1,127 @@
+const questionsWAD = [
+  {
+    answers: ['Python', 'Java', 'C++'],
+    difficulty: '1',
+    question: "Quel langage parmi les suivants est 'faiblement typé' ?",
+  },
+
+  {
+    answers: ['C#', 'Javascript', 'Python'],
+    difficulty: '1',
+    question: "Quel langage parmi les suivants est 'fortement typé' ?",
+  },
+
+  {
+    answers: ['const', 'static', 'define'],
+    difficulty: '1',
+    question: "En C#, pour spécifier que la valeur d'une variable ne peut pas changer, on la déclare comme une constante avec le mot réservé :",
+  },
+
+  {
+    answers: ['//', '?', '()'],
+    difficulty: '1',
+    question: 'Pour avoir un commentaire sur une ligne en C#, Javascript et PHP, on place en début de ligne le(s) caractère(s) :',
+  },
+
+  {
+    answers: ['PHP', 'C#', 'Python'],
+    difficulty: '1',
+    question: 'Quel langage a été créé en 1994 par Rasmus Lerdorf ?',
+  },
+
+  {
+    answers: ['un éléphant', 'une balène', 'une giraffe'],
+    difficulty: '1',
+    question: "Quel est l'animal mascotte de PHP ?",
+  },
+
+  {
+    answers: ['permet de répéter une instruction', "permet d'afficher un message si une erreur est declenchée"],
+    difficulty: '1',
+    question: "Que fait une 'boucle' ?",
+  },
+
+  {
+    answers: ['Java', 'PHP', 'C#'],
+    difficulty: '1',
+    question: "Quel langage on n'apprend pas au cours de la formation WAD chez Interface3 ?",
+  },
+
+  {
+    answers: ['string', 'ulong', 'char'],
+    difficulty: '3',
+    question: "En C#, quel type parmi le suivants n'est pas un 'type primitif' ?",
+  },
+
+  {
+    answers: ['private', 'public', 'protected'],
+    difficulty: '3',
+    question: "Quel est le niveau d'accessibilité par défaut d'une 'class' en C# ?",
+  },
+
+  {
+    answers: ['Console.ReadLine()', 'ExecuteReader()'],
+    difficulty: '3',
+    question: "Travaillant en .NET Framework, qu'est-ce qu'il faut ajouter à la fin du programme pour que la console s'exécute correctement ?",
+  },
+
+  {
+    answers: ['override', 'new', 'static'],
+    difficulty: '3',
+    question: 'En C#, quel mot-clé utilise-t-on pour redéfinir la méthode dans la classe dérivée ? ',
+  },
+
+  {
+    answers: ['false', 'true'],
+    difficulty: '3',
+    question: 'PHP est considéré un langage déclaratif',
+  },
+
+  {
+    answers: ['des serveurs webs', 'des framework Javascript', 'des langages de programmation'],
+    difficulty: '3',
+    question: 'Que sont Apache et IIS ?',
+  },
+
+  {
+    answers: ['true', 'false'],
+    difficulty: '2',
+    question: 'C# est destiné à développer sur la plateforme .NET',
+  },
+
+  {
+    answers: ['Visual Studio', 'Jbuilder', 'Sublime Text'],
+    difficulty: '2',
+    question: "Quel est l'IDE que Microsoft recommande pour développer en C# ?",
+  },
+
+  {
+    answers: ["libérer de l'espace mémoire en déterminant quels o… sont plus utilisés et peuvent donc être éliminés", "éliminer les liens d'héritage qui ne sont plus nécessaires suite à la création d'une interface", 'détecter les erreurs dans le code et déclancher une erreur'],
+    difficulty: '2',
+    question: "Quelle est la fonction du 'garbage collector' ?",
+  },
+
+  {
+    answers: ['false', 'true'],
+    difficulty: '2',
+    question: 'En PHP, toutes les variables doivent être précédées par un # (ex: #myVar)',
+  },
+
+  {
+    answers: ['2002', '2007', '1999'],
+    difficulty: '2',
+    question: 'Dans quelle année Microsoft a présenté la plateforme .NET et le langage C# ?',
+  },
+
+  {
+    answers: ['un framework PHP', 'une librairie Javascript'],
+    difficulty: '2',
+    question: "Qu'est-ce que c'est Symphony ?",
+  },
+
+  {
+    answers: ["echo 'Hello World';", "print_message 'Hello World';", "convertToString 'Hello World';"],
+    difficulty: '2',
+    question: "Comment affiche-t-on 'Hello World' en PHP ?",
+  },
+];
