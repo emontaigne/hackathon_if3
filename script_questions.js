463a444395d891d001305686b2ae50d50316f2bd
alessiaskia <a.scaccia@interface3.be>
2020-11-26 15:30:35 +0100

    add script and liste questions

---
+++ script_questions.js
@@ -0,0 +1,43 @@
+import './style.scss';
+// import $ from 'jquery';
+// import axios from 'axios';
+
+import {
+  allQuestions,
+} from './src/questions_test';
+
+// choisir au hasard un groupe de questions (wad ou web)
+const chosenGroup = allQuestions[Math.floor(Math.random() * allQuestions.length)];
+console.log(chosenGroup);
+
+// parmi les questions, chercher celles qui ont le niveau de difficulté de la case
+const easy = chosenGroup.filter((el) => el.difficulty === '1');
+console.log(easy);
+const medium = chosenGroup.filter((el) => el.difficulty === '2');
+console.log(medium);
+const hard = chosenGroup.filter((el) => el.difficulty === '3');
+console.log(hard);
+
+let levelDifficulty = [];
+const actualCase = '';
+if (actualCase.className('color1')) {
+  levelDifficulty = easy;
+} else if (actualCase.className('color2')) {
+  levelDifficulty = medium;
+} else if (actualCase.className('color3')) {
+  levelDifficulty = medium;
+}
+
+console.log(levelDifficulty);
+
+// au hasard choisir une de ces questions
+
+const chosenQuestion = levelDifficulty[Math.floor(Math.random() * levelDifficulty.length)];
+console.log(chosenQuestion);
+// afficher question + reponses possibles (radio button)
+
+// après click, si la reponse est la bonne
+
+// sinon, si c'est la mauvaise
+
+// faire en sorte que la question lue ne se represente plus
---
+++ src/questions_test.js
@@ -0,0 +1,251 @@
+export const questionsWAD = [
+  {
+    answers: ['Python', 'Java', 'C++'],
+    difficulty: '1',
+    question: "Quel langage parmi les suivants est 'faiblement typé' ?",
+  },
+
+  {
+    answers: ['C#', 'Javascript', 'Python'],
+    difficulty: '1',
+    question: "Quel langage parmi les suivants est 'fortement typé' ?",
+  },
+
+  {
+    answers: ['const', 'static', 'define'],
+    difficulty: '1',
+    question: "En C#, pour spécifier que la valeur d'une variable ne peut pas changer, on la déclare comme une constante avec le mot réservé :",
+  },
+
+  {
+    answers: ['//', '?', '()'],
+    difficulty: '1',
+    question: 'Pour avoir un commentaire sur une ligne en C#, Javascript et PHP, on place en début de ligne le(s) caractère(s) :',
+  },
+
+  {
+    answers: ['PHP', 'C#', 'Python'],
+    difficulty: '1',
+    question: 'Quel langage a été créé en 1994 par Rasmus Lerdorf ?',
+  },
+
+  {
+    answers: ['un éléphant', 'une balène', 'une giraffe'],
+    difficulty: '1',
+    question: "Quel est l'animal mascotte de PHP ?",
+  },
+
+  {
+    answers: ['permet de répéter une instruction', "permet d'afficher un message si une erreur est declenchée"],
+    difficulty: '1',
+    question: "Que fait une 'boucle' ?",
+  },
+
+  {
+    answers: ['Java', 'PHP', 'C#'],
+    difficulty: '1',
+    question: "Quel langage on n'apprend pas au cours de la formation WAD chez Interface3 ?",
+  },
+
+  {
+    answers: ['string', 'ulong', 'char'],
+    difficulty: '3',
+    question: "En C#, quel type parmi le suivants n'est pas un 'type primitif' ?",
+  },
+
+  {
+    answers: ['private', 'public', 'protected'],
+    difficulty: '3',
+    question: "Quel est le niveau d'accessibilité par défaut d'une 'class' en C# ?",
+  },
+
+  {
+    answers: ['Console.ReadLine()', 'ExecuteReader()'],
+    difficulty: '3',
+    question: "Travaillant en .NET Framework, qu'est-ce qu'il faut ajouter à la fin du programme pour que la console s'exécute correctement ?",
+  },
+
+  {
+    answers: ['override', 'new', 'static'],
+    difficulty: '3',
+    question: 'En C#, quel mot-clé utilise-t-on pour redéfinir la méthode dans la classe dérivée ? ',
+  },
+
+  {
+    answers: ['false', 'true'],
+    difficulty: '3',
+    question: 'PHP est considéré un langage déclaratif',
+  },
+
+  {
+    answers: ['des serveurs webs', 'des framework Javascript', 'des langages de programmation'],
+    difficulty: '3',
+    question: 'Que sont Apache et IIS ?',
+  },
+
+  {
+    answers: ['true', 'false'],
+    difficulty: '2',
+    question: 'C# est destiné à développer sur la plateforme .NET',
+  },
+
+  {
+    answers: ['Visual Studio', 'Jbuilder', 'Sublime Text'],
+    difficulty: '2',
+    question: "Quel est l'IDE que Microsoft recommande pour développer en C# ?",
+  },
+
+  {
+    answers: ["libérer de l'espace mémoire en déterminant quels o… sont plus utilisés et peuvent donc être éliminés", "éliminer les liens d'héritage qui ne sont plus nécessaires suite à la création d'une interface", 'détecter les erreurs dans le code et déclancher une erreur'],
+    difficulty: '2',
+    question: "Quelle est la fonction du 'garbage collector' ?",
+  },
+
+  {
+    answers: ['false', 'true'],
+    difficulty: '2',
+    question: 'En PHP, toutes les variables doivent être précédées par un # (ex: #myVar)',
+  },
+
+  {
+    answers: ['2002', '2007', '1999'],
+    difficulty: '2',
+    question: 'Dans quelle année Microsoft a présenté la plateforme .NET et le langage C# ?',
+  },
+
+  {
+    answers: ['un framework PHP', 'une librairie Javascript'],
+    difficulty: '2',
+    question: "Qu'est-ce que c'est Symphony ?",
+  },
+
+  {
+    answers: ["echo 'Hello World';", "print_message 'Hello World';", "convertToString 'Hello World';"],
+    difficulty: '2',
+    question: "Comment affiche-t-on 'Hello World' en PHP ?",
+  },
+];
+export const questionsWEB = [
+  {
+    answers: ['chatAt()', 'blabla'],
+    difficulty: '1',
+    question: 'Which of the following function of String object returns the character at the specified index?',
+  },
+
+  {
+    answers: ['myFunction()', 'blabla'],
+    difficulty: '1',
+    question: "How do you call the function 'myFunction'?",
+  },
+
+  {
+    answers: ['nav', 'blabla'],
+    difficulty: '1',
+    question: 'Which HTML5 element defines navigation links?',
+  },
+
+  {
+    answers: ['8px', 'blabla'],
+    difficulty: '1',
+    question: 'what value is given for the left margin:',
+  },
+
+  {
+    answers: ['true', 'blabla'],
+    difficulty: '1',
+    question: "HTML ID's can only be used once",
+  },
+
+  {
+    answers: ['color', 'blabla'],
+    difficulty: '1',
+    question: 'What property is used to change the text color of an element?',
+  },
+
+  {
+    answers: ['false', 'blabla'],
+    difficulty: '1',
+    question: 'Are CSS property names case-sensitive?',
+  },
+
+  {
+    answers: ['margin:0 auto;', 'blabla'],
+    difficulty: '2',
+    question: 'Which snippet of CSS is commonly used to center a website horizontally?',
+  },
+
+  {
+    answers: ['false', 'blabla'],
+    difficulty: '2',
+    question: 'JavaScript is the same as Java.',
+  },
+
+  {
+    answers: ['id', 'blabla'],
+    difficulty: '2',
+    question: 'The # symbol specifies that the selector is?',
+  },
+
+  {
+    answers: ['!DOCTYPE html', 'blabla'],
+    difficulty: '2',
+    question: 'Which doctype is correct for HTML5?',
+  },
+
+  {
+    answers: ['object', 'blabla'],
+    difficulty: '2',
+    question: "var a = []; What does 'typeof a' return?",
+  },
+
+  {
+    answers: ['script', 'blabla'],
+    difficulty: '2',
+    question: 'Inside which HTML element do we put the JavaScript?',
+  },
+
+  {
+    answers: ['var', 'blabla'],
+    difficulty: '2',
+    question: 'What keyword is used to create a JavaScript variable',
+  },
+
+  {
+    answers: ['double', 'boolean', 'undefined'],
+    difficulty: '2',
+    question: 'Which is not a JavaScript data type?',
+  },
+
+  {
+    answers: ['a library', 'blabla'],
+    difficulty: '3',
+    question: 'What is jQuery?',
+  },
+
+  {
+    answers: ['pop()', 'blabla'],
+    difficulty: '3',
+    question: 'Which of the following function of Array object removes the last element from an array and returns that element?',
+  },
+
+  {
+    answers: ['blink', 'section', 'header'],
+    difficulty: '3',
+    question: 'What is not an HTML5 element?',
+  },
+
+  {
+    answers: ['false', 'blabla'],
+    difficulty: '3',
+    question: 'Does setting margin-top and margin-bottom have an affect on an inline element?',
+  },
+
+  {
+    answers: ['rien', 'blabla'],
+    difficulty: '3',
+    question: 'Que fait un doctype?',
+  },
+
+];
+// eslint-disable-next-line import/prefer-default-export
+export const allQuestions = [questionsWEB, questionsWAD];
